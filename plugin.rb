615c7c4fac2c7697fc720fbe67782ccec7a779c1
Riccardo Terzaghi <87063991+skyterzaghir@users.noreply.github.com>
2022-11-10 19:59:57 +0100

    feat: added macedonian

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# name: discourse-mk--locale
+# about: Adds a new locale for Uzbek
+# version: 0.1
+# authors: Terzaghi Riccardo
+# url: https://github.com/richterzo/discourse-macedonian
+
+register_locale("mk", name: "Macedonian", nativeName: "македонски", fallbackLocale: "en_GB")
+
+after_initialize do
+    if defined? DiscourseTranslator::Google::SUPPORTED_LANG_MAPPING
+      DiscourseTranslator::Google::SUPPORTED_LANG_MAPPING[:mk] = "mk"
+    end
+  end
